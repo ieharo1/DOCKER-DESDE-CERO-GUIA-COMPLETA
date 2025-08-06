4f415c7e7a7245656f8b118ba63b148fd8ff94a8
ieharo1 <zackharo1@gmail.com>
2025-08-06 12:08:50 -0500

    Primer commit

---
+++ Examen/Examen/Models/ciudad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examen.Models
+{
+    public class ciudad
+    {
+        public int id { get; set; }
+        [Display(Name = "Ciudad")]
+        public string nombre_ciudad { get; set; }
+        [Display(Name = "Latitud")]
+        public decimal latitud { get; set; }
+        [Display(Name = "Longitud")]
+        public decimal longitud { get; set; }
+    }
+}
---
+++ Examen/Examen/ViewModels/UsuarioCiudad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Examen.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Examen.ViewModels
+{
+    public class UsuarioCiudad
+    {
+        public SelectList Ciudad { get; set; }
+
+        public user user { get; set; }
+        public string auxiliar { get; set; }
+
+        public SelectList MustraCiudadesCreacion { get; set; }
+        public string ciudadnombre { get; set; }
+
+
+
+    }
+}
